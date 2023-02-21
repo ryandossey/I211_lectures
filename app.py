e798a15ab3b023c1ab72449e4c9e6908e82ae033
rdossey <rdosssey@iu.edu>
2023-02-21 10:57:29 -0500

    class changes

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+with open('dinosaurs.csv', 'r') as csvfile:
+
+   data = csv.DictReader(csvfile)
+
+   dinosaurs = {row['slug']:{'name':row['name'], 'description':row['description'], 'image':row['image'], 'image-credit':row['image-credit'], 'source-url':row['source-url'], 'source-credit':row['source-credit']} for row in data}
+
 @app.route('/')
-def helllo_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', dinosaurs, dinosaurs)
