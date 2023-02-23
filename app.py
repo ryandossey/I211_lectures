986aea0a72887cec34925bb97bd17c4f69d3c957
rdossey <rdosssey@iu.edu>
2023-02-23 10:55:34 -0500

    publish 2

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template, url_for
+import csv
+
 app = Flask(__name__)
 
 with open('dinosaurs.csv', 'r') as csvfile:
-
    data = csv.DictReader(csvfile)
-
    dinosaurs = {row['slug']:{'name':row['name'], 'description':row['description'], 'image':row['image'], 'image-credit':row['image-credit'], 'source-url':row['source-url'], 'source-credit':row['source-credit']} for row in data}
 
 @app.route('/')
-def index():
-    return render_template('index.html', dinosaurs, dinosaurs)
+@app.route('/dino')
+@app.route('/dino/<dino>')
+def index(dino=None):
+   print(dino)
+   if dino and dino in dinosaurs.keys():
+      dinosaur = dinosaurs [dino]
+      return render_template('dino.html', dinosaurs=dinosaurs)
+   else:
+      return render_template('index.html', dinosaurs=dinosaurs)
+    
+
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+if __name__ == "__main__":
+   app.run(debug = True)
