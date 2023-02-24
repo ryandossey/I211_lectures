479eb178ffd640ac85c56429a78dacec8576aa50
rdossey <rdosssey@iu.edu>
2023-02-23 19:31:32 -0500

    faq push

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ with open('dinosaurs.csv', 'r') as csvfile:
    data = csv.DictReader(csvfile)
    dinosaurs = {row['slug']:{'name':row['name'], 'description':row['description'], 'image':row['image'], 'image-credit':row['image-credit'], 'source-url':row['source-url'], 'source-credit':row['source-credit']} for row in data}
 
+with open ('faq.csv', 'r') as csvfile:
+   faq = csv.Reader(csvfile)
+   faq_list = list(faq)
+
 @app.route('/')
 @app.route('/dino')
 @app.route('/dino/<dino>')
@@ -23,6 +27,11 @@ def index(dino=None):
 @app.route('/about')
 def about():
    return render_template('about.html')
+
+
+@app.route('/faq')
+def faq(faq=None):
+   return render_template('faq.html', faq_list=faq_list)
 
 if __name__ == "__main__":
    app.run(debug = True)
