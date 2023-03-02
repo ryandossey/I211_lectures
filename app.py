8d59b1d0f42b4baacda551d62cb5e415e88e1ff3
rdossey <rdosssey@iu.edu>
2023-03-02 09:52:58 -0500

    lecture

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import csv
 
 app = Flask(__name__)
 
+DINO_PATH = app.root_path + '/dinosaurs.csv'
+
+with open(DINO_PATH, 'r') as csvfile:
+   data = csv.DictReader(csvfile)
+   dinosaurs = {row['slug']:{name}}
+
 with open('dinosaurs.csv', 'r') as csvfile:
    data = csv.DictReader(csvfile)
    dinosaurs = {row['slug']:{'name':row['name'], 'description':row['description'], 'image':row['image'], 'image-credit':row['image-credit'], 'source-url':row['source-url'], 'source-credit':row['source-credit']} for row in data}
